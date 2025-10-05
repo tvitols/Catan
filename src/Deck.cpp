974e6d7ad4f47fb6f5d257f8eebb01e640f3b08a
tvitols <tovahn.vitols@gmail.com>
2025-10-04 22:51:20 -0400

    oh wait you have to include things?!?!?!?!

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -5,18 +5,22 @@
 #include "Deck.h"
 
 #include "CustomUtils.h"
-
+#include "KnightCard.h"
+#include "MonopolyCard.h"
+#include "RoadBuilding.h"
+#include "YearOfPlentyCard.h"
+#include "VictoryPointCard.h"
 
 Deck::Deck() {
     for (int i = 0; i < 14; i++) {
-        cards.push_back(new Knight());
+        cards.push_back(new KnightCard());
         if (i <2) {
-            cards.push_back(new Monoply());
+            cards.push_back(new MonopolyCard());
             cards.push_back(new RoadBuilding());
             cards.push_back(new YearOfPlenty());
         }
         if (i < 5) {
-            cards.push_back(new VictoryPointCard(i));
+            cards.push_back(new VictoryPoint(i));
         }
     }
     shuffle();
@@ -24,14 +28,14 @@ Deck::Deck() {
 
 Deck::Deck(int seed) {
     for (int i = 0; i < 14; i++) {
-        cards.push_back(new Knight());
+        cards.push_back(new KnightCard());
         if (i <2) {
-            cards.push_back(new Monoply());
-            cards.push_back(new RoadBuilding());
+            cards.push_back(new MonopolyCard());
+            cards.push_back(new RoadBuilding);
             cards.push_back(new YearOfPlenty());
         }
         if (i < 5) {
-            cards.push_back(new VictoryPointCard(i));
+            cards.push_back(new VictoryPoint(i));
         }
     }
     shuffle(seed);
